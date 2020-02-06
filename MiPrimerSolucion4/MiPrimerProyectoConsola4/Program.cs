cd81e1f686ba37d834f8f8c1b8f711896e66aa34
MaritzaPrado02 <60411627+MaritzaPrado02@users.noreply.github.com>
2020-02-05 22:10:55 -0700

    Initial Commit

---
+++ MiPrimerSolucion4/MiPrimerProyectoConsola4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyectoConsola4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Ingrese un numero: ");
+            a = Convert.ToInt32 (Console.ReadLine());
+            Console.Write("Ingrese un numero: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            c = a + b;
+
+            Console.WriteLine("La suma es: " + c);
+
+            Console.ReadKey();
+
+        }
+    }
+}
