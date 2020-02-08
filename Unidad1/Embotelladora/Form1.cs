bfaffb0eac3f272902d6556bf6a8085bf9cf1804
MaritzaPrado02 <maritzaprado.99cs@gmail.com>
2020-02-08 12:34:20 -0700

    Primer proyecto clase-objeto (botella)

---
+++ Unidad1/Embotelladora/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Embotelladora
+{
+    public partial class frmEmbotelladora : Form
+    {
+        public frmEmbotelladora()
+        {
+            InitializeComponent();
+        }
+
+        private void lblNivelLlenado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            ClaseBotella objBotella = new ClaseBotella();
+            
+
+            objBotella.nivelLlenado = Convert.ToInt32(txtNivelLlenado.Text);
+            MessageBox.Show (objBotella.verificarLlenado());
+
+        }
+    }
+}
