8c6e14691e8a7cb03215f3b40abfa1ab6ce8804c
MaritzaPrado02 <maritzaprado.99cs@gmail.com>
2020-05-05 15:03:06 -0600

    Unidad 5

---
+++ UNIDAD 5/Bidimensional1(2)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bidimensional1_2_
+{
+    //Array de arrays(array de dos dimensiones)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] notas; //Array de dos dimensiones
+            notas = new int[3][]; //Serán 3 bloques de datos
+            notas[0] = new int[10]; //10 notas en un grupo
+            notas[1] = new int[15]; //15 notas en otro grupo
+            notas[2] = new int[12]; //12 notas en el ultimo
+
+            //Damos valores de ejemplo
+            for (int i = 0; i < notas.Length; i++)
+            {
+                for (int j = 0; j < notas[i].Length; j++)
+                {
+                    notas[i][j] = i + j;
+                }
+            }
+
+            //Y mostramos esos valores 
+            for (int i = 0; i < notas.Length; i++)
+            {
+                for (int j = 0; j < notas[i].Length; j++)
+                {
+                    Console.Write(" " +  notas[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UNIDAD 5/Bidimensional2(1)/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Bidimensional2_1_
+{
+    public partial class frmBidimensional : Form
+    {
+        public frmBidimensional()
+        {
+            InitializeComponent();
+        }
+
+        int i, j;
+        String acumArray;
+        int[,] arrayBidi = new int[100, 100];
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtFilas.Text = "";
+            txtColumnas.Text = "";
+            acumArray = "";
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            int filas, columnas;
+            filas = Convert.ToInt16(txtFilas.Text);
+            columnas = Convert.ToInt16(txtColumnas.Text);
+
+            int[,] arrayBidi = new int[10, 10];
+
+            for (int i = 0; i < filas; i++)
+            {
+                acumArray += "\n";
+                for (int j = 0; j < columnas; j++)
+                {
+                    arrayBidi[i, j] = Convert.ToInt16(Interaction.InputBox("Ingresa el valor " + i + ", " + j));
+                    acumArray += arrayBidi[i, j] + ", ";
+                }
+            }
+        }
+
+        private void BtnImprimir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(acumArray, "Elementos del arreglo", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+
+        }
+    }
+}
