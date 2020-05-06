96699c99ed81c678fe5348d2b8e78e403da8ee61
MaritzaPrado02 <maritzaprado.99cs@gmail.com>
2020-05-06 11:27:45 -0600

    Unidad 5

---
+++ UNIDAD 5/NumerosMayorMenor/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace NumerosMayorMenor
+{
+    partial class frmNumeros
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudCantidad = new System.Windows.Forms.NumericUpDown();
+            this.lblSeleccionCantidad = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudCantidad
+            // 
+            this.nudCantidad.Location = new System.Drawing.Point(122, 57);
+            this.nudCantidad.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nudCantidad.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudCantidad.Name = "nudCantidad";
+            this.nudCantidad.Size = new System.Drawing.Size(44, 20);
+            this.nudCantidad.TabIndex = 0;
+            this.nudCantidad.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblSeleccionCantidad
+            // 
+            this.lblSeleccionCantidad.AutoSize = true;
+            this.lblSeleccionCantidad.Location = new System.Drawing.Point(12, 18);
+            this.lblSeleccionCantidad.Name = "lblSeleccionCantidad";
+            this.lblSeleccionCantidad.Size = new System.Drawing.Size(269, 13);
+            this.lblSeleccionCantidad.TabIndex = 1;
+            this.lblSeleccionCantidad.Text = "Seleccione la cantidad de números que desea ingresar:";
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(206, 110);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(104, 165);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 3;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.BtnImprimir_Click);
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Location = new System.Drawing.Point(191, 54);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionar.TabIndex = 4;
+            this.btnSeleccionar.Text = "Seleccionar";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.btnSeleccionar.Click += new System.EventHandler(this.BtnSeleccionar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(88, 112);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 5;
+            this.txtNumero.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(21, 115);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(47, 13);
+            this.lblNumero.TabIndex = 6;
+            this.lblNumero.Text = "Número:";
+            // 
+            // frmNumeros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 211);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnSeleccionar);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.lblSeleccionCantidad);
+            this.Controls.Add(this.nudCantidad);
+            this.Name = "frmNumeros";
+            this.Text = "Números";
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudCantidad;
+        private System.Windows.Forms.Label lblSeleccionCantidad;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnSeleccionar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblNumero;
+    }
+}
+
---
+++ UNIDAD 5/NumerosMayorMenor/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace NumerosMayorMenor
+{
+    public partial class frmNumeros : Form
+    {
+        numeros objNumero;
+        int cantidad;
+        int cont;
+
+        public frmNumeros()
+        {
+            InitializeComponent();
+            btnImprimir.Enabled = false;
+            btnIngresar.Enabled = false;
+            txtNumero.Enabled = false;
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            if (cont <= cantidad)
+            { 
+                objNumero.arregloNumeros[cont] = Convert.ToInt32(txtNumero.Text);
+                cont++;
+                txtNumero.Text = "";
+            }
+            
+            if (cont == cantidad)
+            {
+                MessageBox.Show("Se han registrado los " + cont + " números");
+                btnImprimir.Enabled = true;
+            }
+
+            objNumero.mayor = objNumero.arregloNumeros[0];
+            objNumero.menor = objNumero.arregloNumeros[0];
+            objNumero.mayormenor();
+        }
+
+        private void BtnSeleccionar_Click(object sender, EventArgs e)
+        {
+            objNumero = new numeros();
+            cantidad = (int)nudCantidad.Value;
+            cont = 0;
+            objNumero.arregloNumeros = new int[cantidad];
+            btnIngresar.Enabled = true;
+            txtNumero.Enabled = true;
+
+            nudCantidad.Enabled = false;
+            btnSeleccionar.Enabled = false;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnImprimir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Número mayor: " + objNumero.mayor + "\nNúmero menor: " + objNumero.menor);
+        }
+    }
+}
